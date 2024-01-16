42a5277ec327bdf52d2e1d28295c256766c68ac3
Alex Figliolia <alexfigliolia@gmail.com>
2024-01-15 21:47:38 -0800

    Updates

---
+++ assets/index-PUn-Nq4H.js
@@ -0,0 +1,1 @@
+import{r,j as e}from"./index-IkbQ-jQk.js";import{P as s}from"./index-LTS6MIKP.js";class a extends r.Component{render(){return e.jsx(s,{name:"privacy-policy",children:"Privacy Policy!"})}}export{a as default};
--- portfolio-4/index-ugwA0v08.js
+++
@@ -1,1 +0,0 @@
-import{r,j as e}from"./index-L86DOb5U.js";import{P as s}from"./index-2pxF-Fto.js";class n extends r.Component{render(){return e.jsx(s,{name:"work",children:"Work!"})}}export{n as default};
