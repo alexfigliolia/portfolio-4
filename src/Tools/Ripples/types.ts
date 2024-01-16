d3a4624fc90811a39ccea0c98cf1b6813036191f
Alex Figliolia <alexfigliolia@gmail.com>
2024-01-15 16:27:59 -0800

    Remove jquery from ripples library

---
+++ src/Tools/Ripples/types.ts
@@ -0,0 +1,46 @@
+export interface BrowserConfig {
+  type: number;
+  arrayType: Float32ArrayConstructor | null;
+  linearSupport: boolean;
+  extensions: string[];
+}
+
+export interface IRipples {
+  imageUrl: null | string;
+  resolution: number;
+  dropRadius: number;
+  perturbance: number;
+  interactive: boolean;
+  crossOrigin: string;
+}
+
+export interface Extensions {
+  OES_texture_float: OES_texture_float;
+  OES_texture_half_float: OES_texture_half_float;
+  OES_texture_float_linear: OES_texture_float_linear;
+  OES_texture_half_float_linear: OES_texture_half_float_linear;
+}
+
+export interface Program {
+  id: WebGLProgram;
+  uniforms: Record<string, WebGLUniformLocation>;
+  locations: Record<string, WebGLUniformLocation>;
+}
+
+export interface TargetOffset {
+  top: number;
+  left: number;
+  width: number;
+  height: number;
+}
+
+export interface IStyleCache {
+  zIndex: string;
+  position: string;
+  backgroundWidth: number;
+  backgroundHeight: number;
+  originalInlineCSS: string;
+  originalCSSBackgroundImage: string;
+}
+
+export type StyleCacheKey = keyof IStyleCache;
