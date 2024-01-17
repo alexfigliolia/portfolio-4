38bc17516f80cb341a671bb7e85f6cb618698417
Alex Figliolia <alexfigliolia@gmail.com>
2024-01-16 20:29:58 -0800

    Updates

--- assets/index-1a6o5zFS.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as n}from"./index-xDTG60wb.js";import{P as r}from"./index-dlSp_b6y.js";class t extends e.Component{render(){return n.jsx(r,{name:"contact",children:"Contact!"})}}export{t as default};
