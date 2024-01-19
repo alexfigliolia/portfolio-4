dd336c89fbe35db71e0f5fe1cca6c34bfb45c4f7
Alex Figliolia <alexfigliolia@gmail.com>
2024-01-19 08:44:10 -0800

    Updates

--- assets/index-W0-sF6Ck.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as n}from"./index-bI0ZJl4k.js";import{P as r}from"./index-rG02Ivca.js";class t extends e.Component{render(){return n.jsx(r,{name:"contact",children:"Contact!"})}}export{t as default};
---
+++ assets/index-ra9UwSfi.js
@@ -0,0 +1,1 @@
+import{r as e,j as n}from"./index-zHjotvGk.js";import{P as r}from"./index-glQ_MIRe.js";class t extends e.Component{render(){return n.jsx(r,{name:"contact",children:"Contact!"})}}export{t as default};
